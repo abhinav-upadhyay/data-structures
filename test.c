fc35a9a331b74e00266e61a62c642fc6dadd1ae7
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2013-06-19 20:41:17 +0530

    Store two values in the hashtable, to test out how it does.

--- test.c
+++ test.c
@@ -7,8 +7,11 @@ main(int argc, char **argv)
 {
     hashtable *ht = init(10);
     const char *x = "abhinav";
+    int age = 22;
     put(ht, "name", (void *) x);
-    printf("%s\n", get(ht, "name"));
+    put(ht, "age", (void *) &age); 
+    printf("%s\n", (char *) get(ht, "name"));
+    printf("%d\n", *(int *)get(ht, "age"));
     clear(ht);
     return 0;
            
