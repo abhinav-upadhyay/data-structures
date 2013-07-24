e3ae6dc97faab5f778b2f63cd50547f586e8a996
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2013-07-24 22:41:55 +0530

    Add main.c

---
+++ linkedlist/main.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+static void
+print_callback(void *p)
+{
+    printf("%d\n", *(int *) p);
+}
+
+int
+main(int argc, char **argv)
+{
+    int a1[] = {8, 6, 4};
+    list *l1 = getlist(a1, 3);
+    printf("l1:\n");
+    list_print(l1, print_callback);
+    return 0;
+}
