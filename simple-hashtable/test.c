69a270155ca1af45f820fc40a477bfaa0a7f7d1f
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2013-07-25 01:08:19 +0530

    Test hash collision.

--- simple-hashtable/test.c
+++ simple-hashtable/test.c
@@ -5,13 +5,16 @@
 int
 main(int argc, char **argv)
 {
-    hashtable *ht = init(10);
+    hashtable *ht = init(1);
     const char *x = "abhinav";
     int age = 22;
+    const char *y = "abcd";
     put(ht, "name", (void *) x);
     put(ht, "age", (void *) &age); 
+    put(ht, "random", (void *) y);
     printf("%s\n", (char *) get(ht, "name"));
     printf("%d\n", *(int *)get(ht, "age"));
+    printf("%s\n", (char *) get(ht, "random"));
     clear(ht);
     return 0;
            
